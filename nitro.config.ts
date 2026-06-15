d8942b44c395cc18987c703a1acec9597f20f123
Hridesh Sapkota <hridesh.775421@pmc.tu.edu.np>
2026-06-15 16:04:58 +0545

    Fix Vercel deployment by adding nitro build step

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,11 @@ import { defineNitroConfig } from "nitro/config";
 
 export default defineNitroConfig({
     preset: process.env.NITRO_PRESET || "vercel",
+    srcDir: "src",
+    publicAssets: [
+        {
+            dir: "../dist/client",
+            maxAge: 31536000,
+        },
+    ],
 });
